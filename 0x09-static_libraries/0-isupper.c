69f16604c5728101e242c9323ace6c5583ca9001
NoranSayed <119365828+NoranSayed@users.noreply.github.com>
2023-04-11 01:09:25 +0200

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isupper(int x)
+{
+
+	return (x >= 65 && x <= 90);
+}
