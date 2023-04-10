a190868ce425b22e4ece4813c3e509171170a9f6
NoranSayed <119365828+NoranSayed@users.noreply.github.com>
2023-04-11 01:10:37 +0200

    Create 0-memset.c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *a, char b, unsigned int c)
+{
+	unsigned int d;
+
+	for (d = 0; d < c; d++)
+	{
+		a[d] = b;
+	}
+	return (a);
+}
