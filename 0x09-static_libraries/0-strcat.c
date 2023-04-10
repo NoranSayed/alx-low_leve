af11b6087f2054c869b9fc7cbae609d367e650e5
NoranSayed <119365828+NoranSayed@users.noreply.github.com>
2023-04-11 01:11:59 +0200

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strcat(char *d, char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (d[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s[j] != '\0')
+	{
+		d[i] = s[j];
+		j++;
+		i++;
+	}
+
+	d[i] = '\0';
+	return (d);
+}
